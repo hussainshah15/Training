297721ef28d92dd6161e0efe6f0ca8149d93c3b7
unknown <syedhussainshah52@gmail.com>
2024-04-05 11:35:56 +0500

    new file

--- Training.java
+++ Training.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Training {
 public static void main(String[] args) {
-    
+
+
+    Scanner s1 = new Scanner(System.in);
+    System.out.println("ENter Your name ");
+    String name = s1.nextLine();
+    System.out.println("Welcome to Fit file " + name); 
 
     System.out.println("This is Git hub file ");
+
+
+
 }    
 
 
